4e4b9af3c30c84ff24d6462fd95c1b918cc07269
burny <gamingburny@gmail.com>
2023-08-23 01:21:42 +0200

    Add template withSC2Process

--- main.nim
+++ main.nim
@@ -28,34 +28,31 @@ const ip = "127.0.0.1"
 const port = "38941"
 const cwd = "/media/ssd480/Games/starcraft3/drive_c/Program Files (x86)/StarCraft II/"
 
-echo "Launching sc2"
 var process: SC2Process = SC2Process(ip: ip, port: port, cwd: cwd)
-process.launch
+withSC2Process(process):
+    proc main() {.async.} =
+        # Connect to websocket
+        # https://github.com/BurnySc2/python-sc2/blob/76e4a435732d4359e5bd9e15b6283a0498e212ca/sc2/sc2process.py#L204
+        # https://github.com/treeform/ws#example-client-socket
+        echo "Connecting to websocket"
+        # Not sure why it needs to be like this
+        # https://nim-lang.org/docs/manual.html#types-object-construction
+        let client: ref Client = (ref Client)(process: process)
+        await client.connect
 
-proc main() {.async.} =
-    # Connect to websocket
-    # https://github.com/BurnySc2/python-sc2/blob/76e4a435732d4359e5bd9e15b6283a0498e212ca/sc2/sc2process.py#L204
-    # https://github.com/treeform/ws#example-client-socket
-    echo "Connecting to websocket"
-    # Not sure why it needs to be like this
-    # https://nim-lang.org/docs/manual.html#types-object-construction
-    let client: ref Client = (ref Client)(process: process)
-    await client.connect
+        # Create game (= send which map to load)
+        # https://github.com/BurnySc2/python-sc2/blob/76e4a435732d4359e5bd9e15b6283a0498e212ca/sc2/controller.py#L22C15-L22C26
+        await client.createGame
 
-    # Create game (= send which map to load)
-    # https://github.com/BurnySc2/python-sc2/blob/76e4a435732d4359e5bd9e15b6283a0498e212ca/sc2/controller.py#L22C15-L22C26
-    await client.createGame
+        # Join game as player (map loading will start after this request)
+        await client.joinGame
 
-    # Join game as player (map loading will start after this request)
-    await client.joinGame
+        # Wait a bit
+        sleep(1000)
 
-    # Wait a bit
-    sleep(1000)
-
-    # Close when done
-    client.disconnect
-
-waitFor main()
+        # Close when done
+        client.disconnect
+    waitFor main()
 
 # End process when done
 # I guess on linux this only kills the wine process, not the game?
--- sc2/sc2process.nim
+++ sc2/sc2process.nim
@@ -6,9 +6,10 @@ type SC2Process* = object
     ip*: string
     port*: string
     cwd*: string
-    process*: Process
+    process: Process
 
 proc launch*(p: var SC2Process) =
+    echo "Launching sc2"
     p.process = startProcess(
         "/usr/bin/wine",
         p.cwd,
@@ -17,4 +18,14 @@ proc launch*(p: var SC2Process) =
                 p.port, "-dataDir", p.cwd, "-tempDir", "/tmp/SC2_0peqhatp", ]
     )
 
+proc kill(p: SC2Process) =
+    echo "Killing sc2"
+    p.process.close()
+    discard execCmd("/usr/bin/wineserver -k")
 
+template withSC2Process*(process: var SC2Process, body: untyped): untyped =
+    process.launch
+    try:
+        body
+    finally:
+        process.kill
